96cb4ad9bab6a7ed904304a717b65993fadea093
Nabil <105045605+LekmineNabil@users.noreply.github.com>
2026-04-23 16:44:49 -0400

    Update project layout and create `Flight.java` class with respective `DBConnection.java` class for connection to mysql db on localhost

---
+++ src/travel/DBConnection.java
@@ -0,0 +1,20 @@
+package travel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection get() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/testproject", "testuser", "abc123");
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = get()) {
+            System.out.println("Connected: " + conn.getMetaData().getDatabaseProductVersion());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/travel/model/Flight.java
@@ -0,0 +1,118 @@
+package travel.model;
+
+import java.time.LocalTime;
+
+class Flight {
+
+    private String flightNumber;
+    private String lineID;
+    private String departurePortID;
+    private String destinationPortID;
+    private LocalTime departureTime;
+    private LocalTime arrivalTime;
+    private String flightType; // this will be domestic or international NOT round trip
+    private String daysRunning; // this will be a "bit" mask for a string in the format of 0s and 1s (SMTWTFS)
+    private int seatsTaken;
+    private int craftID;
+
+    public Flight() {
+    }
+
+    public Flight(String flightNumber, String lineID, String departurePortID,
+            String destinationPortID, LocalTime departureTime,
+            LocalTime arrivalTime, String flightType, String daysRunning,
+            int seatsTaken, int craftID) {
+
+        this.flightNumber = flightNumber;
+        this.lineID = lineID;
+        this.departurePortID = departurePortID;
+        this.destinationPortID = destinationPortID;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.flightType = flightType;
+        this.daysRunning = daysRunning;
+        this.seatsTaken = seatsTaken;
+        this.craftID = craftID;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String v) {
+        this.flightNumber = v;
+    }
+
+    public String getLineID() {
+        return lineID;
+    }
+
+    public void setLineID(String v) {
+        this.lineID = v;
+    }
+
+    public String getDeparturePortID() {
+        return departurePortID;
+    }
+
+    public void setDeparturePortID(String v) {
+        this.departurePortID = v;
+    }
+
+    public String getDestinationPortID() {
+        return destinationPortID;
+    }
+
+    public void setDestinationPortID(String v) {
+        this.destinationPortID = v;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime v) {
+        this.departureTime = v;
+    }
+
+    public LocalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalTime v) {
+        this.arrivalTime = v;
+    }
+
+    public String getFlightType() {
+        return flightType;
+    }
+
+    public void setFlightType(String v) {
+        this.flightType = v;
+    }
+
+    public String getDaysRunning() {
+        return daysRunning;
+    }
+
+    public void setDaysRunning(String v) {
+        this.daysRunning = v;
+    }
+
+    public int getSeatsTaken() {
+        return seatsTaken;
+    }
+
+    public void setSeatsTaken(int v) {
+        this.seatsTaken = v;
+    }
+
+    public int getCraftID() {
+        return craftID;
+    }
+
+    public void setCraftID(int v) {
+        this.craftID = v;
+    }
+
+}

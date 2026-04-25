d4f20288975dccbc1b4099d3dbf105d94b548a4c
Nabil <105045605+LekmineNabil@users.noreply.github.com>
2026-04-25 19:13:16 -0400

    Add FlightDAO.java class + method

---
+++ src/travel/dao/FlightDAO.java
@@ -0,0 +1,63 @@
+package travel.dao;
+
+import travel.DBConnection;
+import travel.model.Flight;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class FlightDAO {
+
+    // Invisible Helper Metho
+    private Flight mapRow(ResultSet rs) throws SQLException {
+        Flight f = new Flight();
+        f.setFlightNumber(rs.getString("flightNumber"));
+        f.setLineID(rs.getString("lineID"));
+        f.setDeparturePortID(rs.getString("departure_portID"));
+        f.setDestinationPortID(rs.getString("destination_portID"));
+        f.setDepartureTime(rs.getTime("departureTime").toLocalTime());
+        f.setArrivalTime(rs.getTime("arrivalTime").toLocalTime());
+        f.setFlightType(rs.getString("flightType"));
+        f.setDaysRunning(rs.getString("daysRunning"));
+        f.setSeatsTaken(rs.getInt("seatsTaken"));
+        f.setCraftID(rs.getInt("craftID"));
+        return f;
+    }
+
+    public List<Flight> findAll() {
+        String sql = "SELECT * FROM Flights";
+        ArrayList<Flight> results = new ArrayList<>();
+        try (Connection conn = DBConnection.get();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next())
+                results.add(mapRow(rs));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return results;
+    }
+
+    public List<Flight> findAll() {
+    }
+
+    public Flight findByKey(String flightNumber, String lineID) {
+    }
+
+    public List<Flight> findByRoute(String from, String to) {
+    }
+
+    public void insert(Flight f) {
+    }
+
+    public void update(Flight f) {
+    }
+
+    public void delete(String flightNumber, String lineID) {
+    }
+}

158b6c4b21690723aa4baff8649e9c2fb68a76b2
red801 <gonzalezron37@gmail.com>
2026-04-26 01:09:09 -0400

    Implemented Customer and Employee DAOs for admin, reports wip

---
+++ src/travel/dao/CustomerDAO.java
@@ -0,0 +1,93 @@
+package travel.dao;
+import travel.model.Customer;
+import travel.DBConnection;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class CustomerDAO {
+    
+    private Customer mapRow(ResultSet rs) throws SQLException {
+        Customer c = new Customer();
+        c.setCustomerID(rs.getInt("customerID"));
+        c.setEmail(rs.getString("email"));
+        c.setFirstName(rs.getString("firstName"));
+        c.setLastName(rs.getString("lastName"));
+        c.setPassword(rs.getString("password"));
+        c.setPhoneNumber(rs.getString("phoneNumber"));
+        c.setUsername(rs.getString("username"));
+        return c;
+    }
+
+    public void insert(Customer c) {
+          String sql = """
+                INSERT INTO Customers 
+                (customerID, firstName, lastName, username, password, email, phoneNumber)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+        """;
+            try (Connection connection = DBConnection.get();
+                PreparedStatement ps = connection.prepareStatement(sql)) {
+
+                ps.setInt(1, c.getCustomerID());
+                ps.setString(2, c.getFirstName());
+                ps.setString(3, c.getLastName());
+                ps.setString(4, c.getUsername());
+                ps.setString(5, c.getPassword());
+                ps.setString(6, c.getEmail());
+                ps.setString(7, c.getPhoneNumber());
+
+                ps.executeUpdate(); 
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    
+    public void delete(int id) {
+        String sql =  "DELETE FROM Customers C WHERE C.customerID = ?";
+
+        try (Connection connection = DBConnection.get();
+        PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //potential sysout for "No customer found with ID" 
+    }
+
+    public void update(Customer c) {
+    String sql = """
+        UPDATE Customers
+        SET firstName = ?, lastName = ?, username = ?, 
+            password = ?, email = ?, phoneNumber = ?
+        WHERE customerID = ?
+    """;
+
+    try (Connection connection = DBConnection.get();
+         PreparedStatement ps = connection.prepareStatement(sql)) {
+
+        ps.setString(1, c.getFirstName());
+        ps.setString(2, c.getLastName());
+        ps.setString(3, c.getUsername());
+        ps.setString(4, c.getPassword());
+        ps.setString(5, c.getEmail());
+        ps.setString(6, c.getPhoneNumber());
+        ps.setInt(7, c.getCustomerID());
+
+        ps.executeUpdate();
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+}
+

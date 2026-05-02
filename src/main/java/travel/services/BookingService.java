0224d6b9002f62c4479d318b8705710bb7e202cd
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2026-05-02 16:50:35 -0400

    Update BookingService.java

--- src/main/java/travel/services/BookingService.java
+++ src/main/java/travel/services/BookingService.java
@@ -1,13 +1,16 @@
 package travel.services;
 
-import travel.model.Airport;
+import java.util.List;
 
+import travel.model.*;
+import travel.dao.*;
 
 
 //clear; javac ./src/travel/services/BookingService.java; java -cp "./mysql-connector-j-8.3.0.jar" ./src/travel/services/BookingService.java
 
 public class BookingService {
-    private FlightDao fDao = null;
+    private FlightDAO fDao = null;
+
     public BookingService(){
         fDao = new FlightDAO();
     }
@@ -17,5 +20,6 @@ public class BookingService {
     }
     public static void main(String[] args) {
         //FlightDAO fd = new FlightDAO();
+        new BookingService().getAllFlights();
     }
 }

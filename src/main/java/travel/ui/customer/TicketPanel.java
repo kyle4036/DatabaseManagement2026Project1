a5ed9affebba761c55d5d5c41b01b739084d9898
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2026-05-03 14:32:56 -0400

    for some reason a tag wasn't working in the pom file

--- src/main/java/travel/ui/customer/TicketPanel.java
+++ src/main/java/travel/ui/customer/TicketPanel.java
@@ -2,41 +2,49 @@ package travel.ui.customer;
 
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.math.BigDecimal;
 
-import travel.model.Ticket;
 import travel.ui.*;
 import travel.model.*;
 import travel.services.BookingService;
 
 public class TicketPanel extends JPanel{ 
-    private JLabel ticketInfo = null; 
-    private BookingService bService = null;
-    private Ticket ticket = null;
-    private Customer c = null;
-    private final MainFrame mainFrame;
-    
-    public TicketPanel(Flight flight, MainFrame mainFrame){
-            this.mainFrame = mainFrame;
-            bService = new BookingService();
+       private JLabel ticketInfo = null; 
+       private BookingService bService = null;
+       private Ticket ticket = null;
+       private Customer c = null;
+       private final MainFrame mainFrame;
+       public TicketPanel(MainFrame mainFrame, Flight flight, String TripType){
+              this.mainFrame = mainFrame;
+              bService = new BookingService();
               
-            ticketInfo = new JLabel(flight.getDeparturePortID() + " :" + 
+              ticketInfo = new JLabel(flight.getDeparturePortID() + " :" + 
                                       flight.getDestinationPortID() + " :");
 
-            // ticket = new Ticket(ticketNumber, customerID, purchaseTime, bookingFee, fareCost, tripType, status)
-            JButton buyButton = new JButton("Buy Ticket");
-            buyButton.addActionListener(e -> buyBtnPressed());
+              ticket = new Ticket(
+                     -1,
+                     c.getCustomerID(),
+                     Ticket.getCurrentTime(),
+                     new BigDecimal(25.00),
+                     new BigDecimal(100.00),
+                     TripType,
+                     "Active" 
+              );
 
-            this.add(ticketInfo);
-            this.add(buyButton);
+              JButton buyButton = new JButton("Buy Ticket");
+              buyButton.addActionListener(e -> buyBtnPressed());
+
+              this.add(ticketInfo);
+              this.add(buyButton);
        }
 
-    private void buyBtnPressed(){
-        bService.addTicket(ticket);
-    }
+       private void buyBtnPressed(){
+              bService.addTicket(ticket);
+       }
 
-    @Override
-    public void addNotify() {
-        super.addNotify();
-        Customer c = mainFrame.getCurrentCustomer();
+       @Override
+       public void addNotify() {
+              super.addNotify();
+              Customer c = mainFrame.getCurrentCustomer();
        }
 }

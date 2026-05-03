131b48fdc232951443c8b0ad8696811f088fb907
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2026-05-03 18:20:48 -0400

    Update CustomerSearchFlightPanel.java

--- src/main/java/travel/ui/customer/CustomerSearchFlightPanel.java
+++ src/main/java/travel/ui/customer/CustomerSearchFlightPanel.java
@@ -7,6 +7,10 @@ import java.awt.Font;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+
+import java.awt.GridLayout;
 
 import travel.ui.MainFrame;
 
@@ -28,6 +32,26 @@ public class CustomerSearchFlightPanel extends JPanel {
         add(welcomeLabel, BorderLayout.NORTH);
 
         JLabel title = new JLabel("Search Flights ");
+    }
+
+    private void promptSearch(){
+        JTextField dateField = new JTextField();
+        JTextField timeField = new JTextField();
+        JTextField departureField = new JTextField();
+        JTextField arrivalField = new JTextField();
+
+        JPanel panel = new JPanel(new GridLayout(0,2,8,8));
+
+        panel.add(new JLabel("Date:"));
+        panel.add(dateField);
+        panel.add(new JLabel("Time:"));
+        panel.add(timeField);
+        panel.add(new JLabel("Departure Airport:"));
+        panel.add(departureField);
+        panel.add(new JLabel("Arrival Airport:"));
+        panel.add(arrivalField);
+        
+        int choice = JOptionPane.showConfirmDialog(this, panel, "Search Flights" , JOptionPane.OK_CANCEL_OPTION);
     }
 
 }
